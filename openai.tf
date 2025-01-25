0ebd70bf265c80bb8cc1d5ac38144cfca3036b2d
Michael <gitruon@microsoft.com>
2025-01-25 13:08:48 -0500

    fix

--- openai.tf
+++ openai.tf
@@ -4,8 +4,34 @@ resource "azurerm_cognitive_account" "this" {
   resource_group_name = var.rg_name
   kind                = "OpenAI"
   public_network_access_enabled = var.public_network_access_enabled
-
   sku_name = "S0"
+  identity {
+    type = "SystemAssigned"
+  }
 
   depends_on = [ azurerm_resource_group.this ]
+}
+
+resource "azurerm_role_assignment" "searchcontrib" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Search Service Contributor"
+  principal_id         = azurerm_cognitive_account.this.identity[0].principal_id
+
+  depends_on = [ azurerm_resource_group.this, azurerm_cognitive_account.this ]
+}
+
+resource "azurerm_role_assignment" "searchreader" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Search Index Data Reader"
+  principal_id         = azurerm_cognitive_account.this.identity[0].principal_id
+
+  depends_on = [ azurerm_resource_group.this, azurerm_cognitive_account.this ]
+}
+
+resource "azurerm_role_assignment" "blob" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_cognitive_account.this.identity[0].principal_id
+
+  depends_on = [ azurerm_resource_group.this, azurerm_cognitive_account.this ]
 }
--- search.tf
+++ search.tf
@@ -4,6 +4,28 @@ resource "azurerm_search_service" "this" {
   location            = var.location
   sku                 = "standard"
   public_network_access_enabled = var.public_network_access_enabled
+  local_authentication_enabled = true
+  authentication_failure_mode  = "http403"
+  identity {
+    type = "SystemAssigned"
+  }
 
   depends_on = [ azurerm_resource_group.this ]
+}
+
+resource "azurerm_role_assignment" "aicontrib" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Cognitive Services OpenAI Contributor"
+  principal_id         = azurerm_search_service.this.identity[0].principal_id
+
+  depends_on = [ azurerm_resource_group.this, azurerm_search_service.this ]
+}
+
+
+resource "azurerm_role_assignment" "searchblob" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_search_service.this.identity[0].principal_id
+
+  depends_on = [ azurerm_resource_group.this, azurerm_search_service.this ]
 }
